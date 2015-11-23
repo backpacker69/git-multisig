d19a9935b1d72d0e3b3a002f6e7ecc523cd74acb
dc-tcs <di.chen.ric@gmail.com>
2015-11-22 18:00:32 -0700

    changed default in example config

--- config-example.py
+++ config-example.py
@@ -1,7 +1,7 @@
 import os
 from pubconfig import *
 
-GIT_ENABLED = 1
+GIT_ENABLED = 0
 DATA_DIR = os.path.join(".","flot-operations")
 MY_GIT = "git@github.com:dc-tcs/flot-operations"
 MY_ID = "dc-tcs"
