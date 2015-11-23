74f37cb8cf1da2b2803e4270322aa495f530a91e
dc-tcs <di.chen.ric@gmail.com>
2015-11-22 19:59:38 -0700

    commented config-example.py

--- config-example.py
+++ config-example.py
@@ -1,11 +1,17 @@
 import os
 from pubconfig import *
 
-GIT_ENABLED = 0
 DATA_DIR = os.path.join(".","flot-operations")
+
+GIT_ENABLED = 0
+
+#This is only used with GIT_ENABLED = 1
 MY_GIT = "git@github.com:dc-tcs/flot-operations"
+
+#How others will identify you. Not used yet.
 MY_ID = "dc-tcs"
 
+#SET RPC port
 RPC_PORT = 14002
 #TEST_RPC_PORT = 15002
 #defaults: 14002 for NBT (mainnet)
